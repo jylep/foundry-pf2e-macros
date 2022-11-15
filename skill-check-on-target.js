f03ee75b9a9ed9a0232fca8e8f8c1db59d760d48
jylep <jyl44760@gmail.com>
2022-11-15 20:45:39 +0100

    feat(skill-check): added a dialog, a skill list & forcedDC option

--- skill-check-on-target.js
+++ skill-check-on-target.js
@@ -1,21 +1,22 @@
 const getActor = (token) => token.actor;
-/**
- * @deprecated Implementation will no longer support `actor.data.data`. See Foundry v10 changelog.
- * @param {*} actor A Foundry Actor
- * @returns Actor's data
- */
-const getActorData = (actor) => actor.data.data || actor.data;
+// /**
+//  * @deprecated Implementation will no longer support `actor.data.data`. See Foundry v10 changelog.
+//  * @param {*} actor A Foundry Actor
+//  * @returns Actor's data
+//  */
+// const getActorData = (actor) => actor.data.data || actor.data;
+
 const getUserTargetToken = (user) => {
   // Returns undefined if no target
   const target = user.targets.first();
   return target;
 };
-const getActorLevel = (actorData) => actorData.details.level.value;
+const getActorLevel = (actorData) => actorData.level;
 const getActorDC = (actor) => {
   const DCbyLevel = [14, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40, 42, 44, 46, 48, 50];
-  return DCbyLevel[getActorLevel(getActorData(actor))];
+  return DCbyLevel[getActorLevel(actor)];
 }
-const getActorSkills = (actor) => getActorData(actor).skills;
+const getActorSkills = (actor) => actor.skills;
 const MODIFIER_TYPE = {
   ABILITY: "ability",
   PROFICIENCY: "proficiency",
@@ -25,23 +26,8 @@ const MODIFIER_TYPE = {
   STATUS: "status",
   UNTYPED: "untyped",
 }
-
-const doSkillCheckOnTarget = ({ skillName, skillKey, actionName }) => {
-  const user = game.user;
-  const actor = getActor(token);
-  const target = getUserTargetToken(user);
-
-  if (!actor) {
-    ui.notifications.warn("You must have an actor selected");
-    return;
-  }
-
-  if (!target) {
-    ui.notifications.warn("You must have only one token targeted");
-    return;
-  }
-
-  const tomeModifier = new game.pf2e.Modifier({
+const getTomeRKModifier = () => {
+  return new game.pf2e.Modifier({
     /** The display name of this modifier; can be a localization key (see en.json). */
     label: "Adept Tome Implement",
     /** The actual numeric benefit/penalty that this modifier provides. */
@@ -75,37 +61,79 @@ const doSkillCheckOnTarget = ({ skillName, skillKey, actionName }) => {
     /** Hide this modifier in UIs if it is disabled */
     // hideIfDisabled?: boolean;
   });
-  const modifiers = [tomeModifier];
+}
+/**
+ * Generates a template for all actor's skills
+ * @param {*} skills lists of an actor's skills
+ * @returns the template to inject
+ */
+ const skillSelectorTemplate = (skills) => {
+  const getSkillTemplate = ([name, skill]) => {
+    return `
+      <li id="skill@${skill.slug}/${skill.label}" class="directory-item document macro flexrow">
+        <h4 class="document-name" style="text-align:center;">
+          <i class="fas fa-hammer" style="margin-right:1em;"></i>
+          <a>${game.i18n.localize(skill.label)}</a>
+        </h4>
+      </li>
+    `;
+  };
+
+  return `
+  <section class="directory flexcol" style="padding:1em;">
+    <ol style="
+      margin: 0px 1em;
+      padding: 0;
+      border: 2px solid var(--color-border-dark);
+    ">
+      ${Object.entries(skills).map(getSkillTemplate).join('\n')}
+    </ol>
+  </section>
+`;
+};
+
+
+// ========== Main functions under this section ================
+
+/**
+ * @param {*} param An object containing keys to setup the PF2E Check
+ * @returns A standard DC skill check against a targeted token actor
+ */
+const doSkillCheckOnTarget = ({ skillName, skillKey, actionName, modifiers = [], forcedDC = undefined }) => {
+  const user = game.user;
+  const target = getUserTargetToken(user);
+
+  if (!actor) {
+    ui.notifications.warn("You must have an actor selected");
+    return;
+  }
+
+  if (!target) {
+    ui.notifications.warn("You must have only one token targeted");
+    return;
+  }
+
   const rollTitleTemplate = `<span class="pf2-icon">A</span> <b>${actionName}</b> - <p class="compact-text">${skillName} Skill Check</p>`;
   const rollModifier = new game.pf2e.CheckModifier(
     rollTitleTemplate,
     getActorSkills(actor)[skillKey],
     modifiers
   );
-
-  const skillNotes = [...getActorSkills(actor)[skillKey].notes];
   const rollType = "skill-check"
   const rollOptions = actor.getRollOptions(['all', rollType, skillName.toLowerCase()]);
-  
-
   const rollContext = {
     actor: actor,
     type: rollType,
     options: rollOptions,
-    notes: skillNotes,
     title: `${actionName} - ${skillName}`,
     dc: {
-      value: getActorDC(getActor(getUserTargetToken(user)))
+      value: forcedDC ?? getActorDC(getActor(getUserTargetToken(user)))
     }
   };
 
   const rollCallback = (roll, outcome, message) => {
-    // TODO: Remove the log
-    console.log("rollResult", [
-      roll,
-      outcome,
-      message
-    ]);
+    // if you need to do something with the roll result
+    // do it here.
   }
 
   // Find documentation on this here: https://github.com/foundryvtt/pf2e/blob/84ae47cb03fc79a5c1be4e31f27db01202068c13/src/module/system/rolls.ts#L160
@@ -116,12 +144,50 @@ const doSkillCheckOnTarget = ({ skillName, skillKey, actionName }) => {
     rollCallback
   );
 }
+/**
+ * Renders a list of skills for current actor and makes a skill check against
+ * the actor's target
+ */
+const pickSkillAndDoCheck = ({ actionName, modifiers = [], forcedDC = undefined }) => {
+  Hooks.once('renderDialog', (dialog, html, contentAndButtons) => {
+    const liElements = Array.from(html[0].getElementsByTagName("li"));
+    liElements.forEach(el => {
+      // Assign a callback using the skill label and skill slug 
+      const [skillKey, skillName] = el.id.split('@').pop().split('/');
+      el.onclick = (e) => {
+        e.preventDefault();
+        dialog.close(); // Dialog no longer needed
+        doSkillCheckOnTarget({ skillName, skillKey, actionName, modifiers, forcedDC })
+      }
+    });
+  })
 
-const skillName = "Esoteric Lore";
-const skillKey = "esoteric-lore";
-const actionName = "Recall Knowledge";
-doSkillCheckOnTarget({ skillName, skillKey, actionName });
+  new Dialog({
+    title: 'Choose a skill',
+    content: skillSelectorTemplate(getActorSkills(actor)),
+    buttons: {
+      cancel: {
+        icon: '',
+        label: 'Cancel',
+      }
+    },
+    default: 'cancel'
+  }, {
+    height: 650,
+    resizable: true,
+    scrollY: ["directory"]
+  }).render(true);
+}
 
+// ========== Example usage under this section ================
 
-// const dialogContent = `
-// `;
+// DC is also adjustable with the forcedDC option
+// pickSkillAndDoCheck({ actionName: "Recall Knowledge", forcedDC: 5 });
+pickSkillAndDoCheck({ actionName: "Recall Knowledge" });
+
+// doSkillCheckOnTarget({
+//   skillName: 'Esoteric Lore',
+//   skillKey: 'esoteric-lore',
+//   actionName: 'Recall Knowledge',
+//   modifiers: [getTomeRKModifier()]
+// })
